da8bae6af8ee276ab5e09ade69d0a9d64797798c
DavidPPizarro <paulpizarro151@gmail.com>
2024-11-24 19:36:10 -0500

    correct errors

--- manage.py
+++ manage.py
@@ -15,9 +15,9 @@ def main():
             django.setup()
             from django.contrib.auth.models import User
 
-            username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
-            email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
-            password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'adminpassword')
+            username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'adminDavid')
+            email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'paulpizarro151@gamil.com')
+            password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'con12,ro!da')
 
             if not User.objects.filter(username=username).exists():
                 User.objects.create_superuser(username=username, email=email, password=password)
